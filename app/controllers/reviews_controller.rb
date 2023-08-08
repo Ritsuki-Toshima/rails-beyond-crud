9da5c1bd529af5db508860520e05a709709376a2
Ritsuki Toshima <ricky0912t@gmail.com>
2023-08-09 00:56:41 +0900

    done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.resutaurant_id = @restaurant.id
+    if @review.save
+      redirect_to restauran_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "/restaurants/top", to: "restaurants#top"
 end
